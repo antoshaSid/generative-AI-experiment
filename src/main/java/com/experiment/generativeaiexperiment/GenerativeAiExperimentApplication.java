d2b86e01f27c697930061e25f345281261e48ae6
asidliar <asidliar@atlassian.com>
2023-08-23 14:05:50 +0300

    UC#1 Infrastructure setup

---
+++ src/main/java/com/experiment/generativeaiexperiment/GenerativeAiExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.experiment.generativeaiexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerativeAiExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerativeAiExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experiment/generativeaiexperiment/GenerativeAiExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.generativeaiexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerativeAiExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
